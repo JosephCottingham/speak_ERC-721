49a313b0deb0c45767af7624cc72c28e1bc11134
Joseph Cottingham <joseph.cottingham@protonmail.com>
2021-07-13 18:00:06 -0400

    comment out test

--- speak/contracts/tests/TestSpeakWordManager.sol
+++ speak/contracts/tests/TestSpeakWordManager.sol
@@ -1,39 +1,39 @@
-pragma solidity ^0.8.0;
+// pragma solidity ^0.8.0;
 
-import "truffle/Assert.sol";
-import "truffle/DeployedAddresses.sol";
-import "../SpeakWordManager.sol";
+// import "truffle/Assert.sol";
+// import "truffle/DeployedAddresses.sol";
+// import "../SpeakWordManager.sol";
 
-contract TestSpeakWordManager {
+// contract TestSpeakWordManager {
 
-  Background public background;
+//   Background public background;
 
-    // Run before every test function
-    function beforeEach() public {
-        background = new Background();
-    }
+//     // Run before every test function
+//     function beforeEach() public {
+//         background = new Background();
+//     }
 
-    // Test that it stores a value correctly
-    function testItcreateWord() public {
-        uint value = 5;
-        background.createWord("title", "body");
-        // uint result = background.getValue(0);
-        // Assert.equal(result, value, "It should store the correct value");
-    }
+//     // Test that it stores a value correctly
+//     function testItcreateWord() public {
+//         uint value = 5;
+//         background.createWord("title", "body");
+//         // uint result = background.getValue(0);
+//         // Assert.equal(result, value, "It should store the correct value");
+//     }
 
-    // Test that it gets the correct number of values
-    function testItGetTopWords() public {
-        background.storeValue(99);
-        (uint256[50] memory wordIdArray, string[50] memory titleArray, string[50] memory bodyArray, uint32[50] memory timestampArray, uint256[50] memory totalLikesArray, bool[50] memory likedArray) = background.getTopWords();
-    }
+//     // Test that it gets the correct number of values
+//     function testItGetTopWords() public {
+//         background.storeValue(99);
+//         (uint256[50] memory wordIdArray, string[50] memory titleArray, string[50] memory bodyArray, uint32[50] memory timestampArray, uint256[50] memory totalLikesArray, bool[50] memory likedArray) = background.getTopWords();
+//     }
 
-    // // Test that it stores multiple values correctly
-    // function testItStoresMultipleValues() public {
-    //     for (uint8 i = 0; i < 10; i++) {
-    //         uint value = i;
-    //         background.storeValue(value);
-    //         uint result = background.getValue(i);
-    //         Assert.equal(result, value, "It should store the correct value for multiple values");
-    //     }
-    // }
-}
+//     // // Test that it stores multiple values correctly
+//     // function testItStoresMultipleValues() public {
+//     //     for (uint8 i = 0; i < 10; i++) {
+//     //         uint value = i;
+//     //         background.storeValue(value);
+//     //         uint result = background.getValue(i);
+//     //         Assert.equal(result, value, "It should store the correct value for multiple values");
+//     //     }
+//     // }
+// }
